47df54f09ed176a3f2dec969d6ed16ef4691cbc7
David92p <dave.panetta92@gmail.com>
2023-04-13 12:09:58 +0200

    add error main component

---
+++ src/components/main/ErrorMain.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorMain = () => {
+  return (
+    <div className="container-error-main">
+      <h1>Oops... Something went wrong</h1>
+    </div>
+  );
+};
+
+export default ErrorMain;
