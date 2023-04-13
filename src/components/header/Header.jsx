b03f03a228f40a50a49f9bc9388bcddfc2afe7f3
David92p <dave.panetta92@gmail.com>
2023-04-13 09:51:08 +0200

    add state data input

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Main from "../main/Main";
 
 const Header = () => {
+  const [namePokemon, setNamePokemon] = useState(null);
+  const [dataPokemon, setDataPokemon] = useState(null);
+
+  const inputValue = (e) => {
+    setNamePokemon(e.target.value);
+  };
+
+  const dataInput = async () => {
+    // console.log(namePokemon);
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${namePokemon}`
+      );
+      if (!response.ok) {
+        const msg = `There was an error ${response.status} ${response.statusText}`;
+        throw new Error(msg);
+      }
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container-header">
-      <input type="text" className="text-input" />
-      <button className="btn">Ricerca</button>
+      <input type="text" className="text-input" onChange={inputValue} />
+      <button className="btn" onClick={dataInput}>
+        Ricerca
+      </button>
     </div>
   );
 };
